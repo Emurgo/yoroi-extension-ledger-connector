f6fe9e040a08db244b96f2ca2b483774d69d2570
Sebastien Guillemot <sebastiengllmt@gmail.com>
2021-04-27 11:18:59 +0900

    4.3.0: update to v3 URL

--- src/const.js
+++ src/const.js
@@ -8,8 +8,7 @@ import { CONNECTION_TYPE } from './types';
 // https://github.com/satoshilabs/slips/blob/master/slip-0044.md
 export const HARDENED = 0x80000000;
 
-// Note: we connect to "v2" for Shelley
-export const CONNECTOR_URL = 'https://emurgo.github.io/yoroi-extension-ledger-connect/#/v2';
+export const CONNECTOR_URL = 'https://emurgo.github.io/yoroi-extension-ledger-connect/#/v3';
 export const YOROI_LEDGER_CONNECT_TARGET_NAME = 'YOROI-LEDGER-CONNECT' + (
   chrome.runtime.id != null
     ? ('-' + chrome.runtime.id)
